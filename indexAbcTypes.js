86ab5b228c9076250e0beb4294fb0e1e6afc440f
Paul Puey <paul@airbitz.co>
2017-09-11 23:49:36 -0700

    Add JSON schemas

--- indexAbcTypes.js
+++ indexAbcTypes.js
@@ -307,3 +307,97 @@ export interface AbcExchangePluginFactory {
   pluginType: 'exchange',
   static makePlugin(opts: { io: AbcIo }): Promise<AbcExchangePlugin>
 }
+
+// JSON API schemas ---------------------------------------------------
+
+export const AbcEthFeesSchema = {
+  'type': 'object',
+  'additionalProperties': {
+    'type': 'object',
+    'properties': {
+      'gasLimit': {
+        'type': 'object',
+        'properties': {
+          'regularTransaction': { 'type': 'string' },
+          'tokenTransaction': { 'type': 'string' }
+        },
+        'required': [ 'regularTransaction', 'tokenTransaction' ]
+      },
+      'gasPrice': {
+        'type': 'object',
+        'properties': {
+          'lowFee': { 'type': 'string' },
+          'standardFeeLow': { 'type': 'string' },
+          'standardFeeHigh': { 'type': 'string' },
+          'standardFeeLowAmount': { 'type': 'string' },
+          'standardFeeHighAmount': { 'type': 'string' },
+          'highFee': { 'type': 'string' }
+        },
+        'required': [ 'lowFee', 'standardFeeLow', 'standardFeeHigh', 'standardFeeLowAmount', 'standardFeeHighAmount', 'highFee' ]
+      }
+    },
+    'required': [ 'gasLimit' ]
+  }
+}
+
+export const AbcCurrencyInfoSchema = {
+  'type': 'object',
+  'properties': {
+    'walletTypes': {
+      'type': 'array',
+      'items': {'type': 'string'}
+    },
+    'currencyCode': { 'type': 'string' },
+    'currencyName': { 'type': 'string' },
+    'addressExplorer': { 'type': 'string' },
+    'transactionExplorer': { 'type': 'string' },
+    'denominations': {
+      'type': 'array',
+      'items': {
+        'type': 'object',
+        'properties': {
+          'name': { 'type': 'string' },
+          'multiplier': { 'type': 'string' },
+          'symbol': { 'type': 'string' }
+        },
+        'required': [ 'name', 'multiplier' ]
+      }
+    },
+    'symbolImage': { 'type': 'string' },
+    'metaTokens': {
+      'type': 'array',
+      'items': {
+        'type': 'object',
+        'properties': {
+          'currencyCode': { 'type': 'string' },
+          'currencyName': { 'type': 'string' },
+          'denominations': {
+            'type': 'array',
+            'items': {
+              'type': 'object',
+              'properties': {
+                'name': { 'type': 'string' },
+                'multiplier': { 'type': 'string' },
+                'symbol': { 'type': 'string' }
+              },
+              'required': [ 'name', 'multiplier' ]
+            }
+          },
+          'contractAddress': { 'type': 'string' },
+          'symbolImage': { 'type': 'string' }
+        },
+        'required': [ 'currencyCode', 'currencyName', 'denominations' ]
+      }
+    }
+  },
+  'required': [
+    'walletTypes',
+    'currencyCode',
+    'currencyName',
+    'defaultSettings',
+    'denominations',
+    'symbolImage',
+    'addressExplorer',
+    'transactionExplorer'
+  ]
+}
